c1fb583c8b3f549bdf8ecf083ff1cfc02c41c079
Waseb Untwale <93330099+Waseb-untwale@users.noreply.github.com>
2025-04-21 16:24:22 +0530

    undo --next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com", "localhost"], // Allow both Cloudinary and localhost images
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Client-side-specific configuration
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        crypto: require.resolve('crypto-browserify'),
-        stream: require.resolve('stream-browserify'),
-        buffer: require.resolve('buffer/'),
-        fs: false,
-        os: false,
-        path: false,
-      };
-    }
-    return config;
+    domains: ["res.cloudinary.com", "localhost"], // ✅ Allow both Cloudinary and localhost images
   },
 };
 
